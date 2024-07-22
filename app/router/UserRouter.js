13ddd1996de97354afad7421cc946ca449daaf37
chouchenechadha1 <chouchenechadha01@gmail.com>
2024-07-22 16:42:48 +0200

    first day

--- app/router/UserRouter.js
+++ app/router/UserRouter.js
@@ -3,7 +3,11 @@ const express = require('express');
 const router = express.Router();
 const User = require('../controllers/Usercontroller');
 const { validateUserSignUp, userValidation } = require('../middlewares/validator/User');
+const { isAuth } = require('../middlewares/auth');
   
 router.post('/create-user',validateUserSignUp,userValidation ,User.createUser);
 router.post('/sign-in',User.userSignIn ,User.userSignIn);
+router.post('create-post',isAuth,(req,res)=>{
+    res.send('welcome to the application')
+})
 module.exports = router;
